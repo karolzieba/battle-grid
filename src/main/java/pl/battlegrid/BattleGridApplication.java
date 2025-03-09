5f1fe20d7bffe33d801e3ec44da1a1aab7176cd7
Karol Zięba <kz516749@gmail.com>
2025-03-09 18:20:39 +0100

    Initial commit.

---
+++ src/main/java/pl/battlegrid/BattleGridApplication.java
@@ -0,0 +1,13 @@
+package pl.battlegrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleGridApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BattleGridApplication.class, args);
+    }
+
+}
